ac321ce042561a023ca4721af911b6a7f591325c
Balves129 <b.balves129@gmail.com>
2022-06-30 16:03:46 -0300

    breno

--- main.py
+++
@@ -1,9 +0,0 @@
-#from classe_contato import*
-#from classe_agenda import *
-from classe_Menu import *
-
-#agenda = Agenda()
-#agenda.salvar_contatos()
-#agenda.listar_todos_contatos()
-
-menu_de_opções = Menu()
